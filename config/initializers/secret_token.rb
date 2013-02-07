d3aba1aaf8cf977de49e725976642b7eb7432167
Tipper Llaguno <tipperllaguno@gmail.com>
2013-02-06 17:35:23 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingApp::Application.config.secret_token = 'f42eacaa719d1c2c704da7ca2da0324cc186870d81dab40a98343b8c4926c4adcaa7db4054b6670339f011d42c64ddc0e86376cc379bd77c92bb728d2f5f4d70'
